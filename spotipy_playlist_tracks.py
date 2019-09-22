92f339b81af8476e9e7cf78f4927f0897df855c4
yung-pietro <peter.imai@gmail.com>
2019-09-22 10:09:57 -0500

    simplified the loop, grabbing the info from the results + attributes in the same pass

--- spotipy_playlist_tracks.py
+++ spotipy_playlist_tracks.py
@@ -34,13 +34,15 @@ results = sp.user_playlist_tracks(username, playlist_id, fields=None, limit=100,
 
 # print the type of the object 
 # print(type(results['items']))
-track_ids = []
+
 
 # create a dictionary with ID and attributes of a single track
 
-track_attributes = {'track_name': [], 
-                    'track_id': [], 
-                    'track_url': [], 
+track_attributes = { 
+                    'track_name': [], 
+                    'track_id': [],
+                   # 'track_url': [], 
+                    'album_art': [],
                     'danceability': [], 
                     'energy': [], 
                     'key': [], 
@@ -48,51 +50,59 @@ track_attributes = {'track_name': [],
                     'mode': [], 
                     'speechiness': [],
                     'acousticness': [],
-                    'instrumental': [],
+                    'instrumentalness': [],
                     'liveness': [],
                     'valence': [],
                     'tempo': [],
-                    'duration_ms': [] }
+                    'duration_ms': []} 
 
-id_list_hack = []
 
 for item in results['items']:
-    # print(type(item))
-    #determine what type of element each item is
-
-    #print(item['track']['id'])
-    #prints a list of track ids
-
-    #track_ids.append(item['track']['id'])
-    # appending track ids to the list we created earlier
-
+    # id_list.append(item['track']['id'])
+    track_id = item['track']['id']
     track_attributes['track_name'].append(item['track']['name'])
-    track_attributes['track_id'].append(item['track']['id'])
-    track_attributes['track_url'].append(item['track']['external_urls']['spotify'])
-    id_list_hack.append(item['track']['id'])
+    #track_attributes['track_url'].append(item['track']['url'])
+    track_attributes['album_art'].append(item['track']['album']['images'][1]['url'])
+    ind_track_attributes = sp.audio_features(track_id)[0]
+        #the [0] accesses the nested dictionary inside the list
+    track_attributes['track_id'].append(ind_track_attributes['id'])
+    track_attributes['danceability'].append(ind_track_attributes['danceability'])
+    track_attributes['energy'].append(ind_track_attributes['energy'])
+    track_attributes['key'].append(ind_track_attributes['key'])
+    track_attributes['loudness'].append(ind_track_attributes['loudness'])
+    track_attributes['mode'].append(ind_track_attributes['mode'])
+    track_attributes['speechiness'].append(ind_track_attributes['speechiness'])
+    track_attributes['acousticness'].append(ind_track_attributes['acousticness'])
+    track_attributes['instrumentalness'].append(ind_track_attributes['instrumentalness'])
+    track_attributes['liveness'].append(ind_track_attributes['liveness'])
+    track_attributes['valence'].append(ind_track_attributes['valence'])
+    track_attributes['tempo'].append(ind_track_attributes['tempo'])
+    track_attributes['duration_ms'].append(ind_track_attributes['duration_ms'])
 
-# perhaps, first build the dictionary, then add things like above
+for key in track_attributes:
+    print(len(track_attributes[key]))
+    print(key)
+# print(track_attributes)
 
-list = 0
-for id in id_list_hack:
-    att_add = sp.audio_features(id)
-    track_attributes[list]['energy'].append(att_add['energy'])
-    track_attributes[list]['key'].append(att_add['key'])
-    track_attributes[list]['loudness'].append(att_add['loudness'])
-    track_attributes[list]['mode'].append(att_add['mode'])
-    track_attributes[list]['speechiness'].append(att_add['speechiness'])
-    track_attributes[list]['acousticness'].append(att_add['acousticness'])
-    track_attributes[list]['instrumental'].append(att_add['instrumental'])
-    track_attributes[list]['liveness'].append(att_add['liveness'])
-    track_attributes[list]['valence'].append(att_add['valence'])
-    track_attributes[list]['tempo'].append(att_add['tempo'])
-    track_attributes[list]['duration_ms'].append(att_add['duration'])
-    list += 1
+# for key, value in track_attributes.items():
+#     print(len(value))
+# exit()
+
+# test = sp.audio_features(id_list[0])
+# print(f"Features of {id_list[0]} are: ")
+# print(test)
+
+
+    # track_attributes['track_name'].append(item['track']['name'])
+    # track_attributes['track_id'].append(item['track']['id'])
+    # track_attributes['track_url'].append(item['track']['external_urls']['spotify'])
+    #id_list_hack.append(item['track']['id'])
+
 
 df = pd.DataFrame.from_dict(track_attributes)
 print(df)
 df.to_csv(r'track_attributes.csv', index = False)
-   
+exit() 
     #print(id)
     #print(sp.audio_features(id)[0]['loudness'])
 
