2d313ab600553ec1b645f8c4b6e20bb28d08873a
yung-pietro <peter.imai@gmail.com>
2019-10-17 14:15:01 -0700

    abstract credentials to json file

--- spotipy_playlist_tracks.py
+++ spotipy_playlist_tracks.py
@@ -1,23 +1,22 @@
-import os
-import sys
+import json
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from pprint import pprint
 import pandas as pd
 
-print(os.path.abspath(os.path.curdir))
+with open('config.json') as config_file:
+    data = json.load(config_file)
 
-exit()
-client_id = "866e67d3c5924baca1114ed5bc4eef50"
-client_secret = "c6e2baa4ecac47cc9ecd6bf7dced31f4"
+client_id = data['id']
+client_secret = data['secret']
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-username = "1252709106"
-playlist_id = '37i9dQZF1E8Gd0iQSccTIC'
-offset = 0
+username = data['username']
+playlist_id = data['playlist_id']
+offset = data['offset']
 
 
 results = sp.user_playlist_tracks(username, playlist_id, fields=None, limit=100, offset=offset, market=None)
